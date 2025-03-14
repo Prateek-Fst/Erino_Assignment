a34ccfcbc65026802b00847df716e2817f4292ed
Prateek-Fst <147906999+Prateek-Fst@users.noreply.github.com>
2025-03-14 13:03:17 +0530

    updated env

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://erino-assignment-zeta.vercel.app/', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
